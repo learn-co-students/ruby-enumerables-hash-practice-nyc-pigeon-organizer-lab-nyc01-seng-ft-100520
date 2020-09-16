bd187369e4e0a3917e48b1528449eace105df6c8
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-16 22:35:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,56 @@
+require 'pry'
+
+pigeon_data = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  results = data.each_with_object({}) do |(key, value), name_hash|
+      value.collect do |key2, value2|
+        value2.collect do |name|
+         if !name_hash[name] 
+           name_hash[name] = {}
+         end
+      if !name_hash[name][key]
+        name_hash[name][key] = []
+        # binding.pry
+      end
+      name_hash[name][key].push(key2.to_s)
+      # binding.pry
+    end
+  end
 end
+
+end
+
+
+
+
+
+
+# push data into an array, then turn array into hash??
+
+
+
+# counter = 0 
+# while counter < name_hash.length do 
+# data.each do |key, value|
+#     value.each do |key2, value2|
+#       if value2 include? name_hash[counter] do 
+#         name_hash[counter] = key2
+#       end
+#       counter += 1
+#   end
+# end 
+# end 
