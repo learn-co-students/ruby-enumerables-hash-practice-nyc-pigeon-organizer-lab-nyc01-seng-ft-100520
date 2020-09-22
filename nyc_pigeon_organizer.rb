1ce9b9e537a64f6c5f729e1fab68a28e35e538a3
Jonathan Hakimian <jonnyhak@gmail.com>
2020-09-22 15:05:01 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+
+  data.each do |outer_key, sub_hash|
+    sub_hash.each do |inner_key, name|
+      name.each do |ele|
+        if !hash[ele]
+          hash[ele] = {}
+        end
+        if !hash[ele][outer_key]
+          hash[ele][outer_key] = []
+        end
+        hash[ele][outer_key].push(inner_key.to_s)
+      end
+    end
+  end 
+  hash
 end
