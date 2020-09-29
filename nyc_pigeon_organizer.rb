7546732ddb3750aa8456a60fa1f8a921d9123c31
Steven wu <stevenwu91@icloud.com>
2020-09-29 13:32:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  new_hash = {}
+  
+  data.each do |key,value|
+    value.each do |value2 , names|
+      names.each do |name|
+        
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+       
+        new_hash[name][key] << value2.to_s
+      
+      end
+    
+    end
+  end
+  new_hash
+  
 end
+
